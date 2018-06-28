14a40a2e0183c926da7e9546094ad04e58858557
The Mai <Mai.The.09@gmail.com>
2018-06-28 04:09:55 -0500

    add techlogos and finish projects

--- data/projects.js
+++ data/projects.js
@@ -1,5 +1,16 @@
-const react = 'http://via.placeholder.com/100x50';
-const node = 'http://via.placeholder.com/125x50';
+const node = './assets/images/techlogos/node.png';
+const react = './assets/images/techlogos/react.png';
+const mongodb = './assets/images/techlogos/mongodb.png';
+const mongoose = './assets/images/techlogos/mongoose.png';
+const heroku = './assets/images/techlogos/heroku.png';
+const mysql = './assets/images/techlogos/mysql.png';
+const jquery = './assets/images/techlogos/jquery.png';
+const materialize = './assets/images/techlogos/materialize.png';
+const firebase = './assets/images/techlogos/firebase.png';
+const sequelize = './assets/images/techlogos/sequelize.png';
+const knockout = './assets/images/techlogos/knockout.png';
+const bootstrap = './assets/images/techlogos/bootstrap.png';
+const handlebars = './assets/images/techlogos/handlebars.png';
 const projects = [
 	{
 		direction: 'left',
@@ -9,19 +20,21 @@ const projects = [
 		text: 'WOW! This is some really well-written text about what technologies this app incorporates and how it is used! It makes me looks REALLY REALLY impressive because I use very greasy and slick words. NICE JOB ME!',
 		media: 'http://via.placeholder.com/600x400',
 		github: 'https://github.com/thaiscmky/phonow',		
-		livelink: 'https://phosho-phonow-production.herokuapp.com/',
-		technologies: [react, node, node],
+		applink: 'https://phosho-phonow-production.herokuapp.com/',
+		customerlink: 'https://phosho-phonow-production.herokuapp.com/',
+		technologies: [node, react, materialize, mongodb, mongoose, heroku],
 	},
 	{
 		direction: 'right',
 		color: 'grey',
 		title: 'Pho Now | CMS',
 		subtitle: 'A trendy restaurant website on the customer face side with a back-end administrative side that acts as a Content Management System for the restaurant owner.',
-		text: 'Pho Now uses Bootstrap4, HandleBars, and jQuery to deliver information to visitors. The site\'s pulls dynamic data from a MySQL DB using Sequalize as an ORM. Using the CMS\'s UI the owner can make live changes to the customer facing front-end. A Node.js server is hosted on Heroku along with a JawsDB.',
+		text: 'Pho Now uses Bootstrap4, HandleBars, and jQuery to deliver information to visitors. The site\'s pulls dynamic data from a MySQL DB using Sequelize as an ORM. Using the CMS\'s UI the owner can make live changes to the customer facing front-end. A Node.js server is hosted on Heroku along with a JawsDB.',
 		media: 'http://via.placeholder.com/600x400',
 		github: 'https://github.com/thaiscmky/phonow',		
-		livelink: 'https://phosho-phonow-production.herokuapp.com/',
-		technologies: [react, node, node],
+		applink: 'https://phosho-phonow-production.herokuapp.com/admin',
+		customerlink: 'https://phosho-phonow-production.herokuapp.com/',
+		technologies: [node, bootstrap, handlebars, jquery, mysql, sequelize, heroku],
 	},
 	{
 		direction: 'left',
@@ -31,8 +44,8 @@ const projects = [
 		text: 'Let\'s Eat uses Bootstrap4 as a base for styling. A combination of Knockout.js and jQuery is used for page manipulation. The app is integrated with Google Maps API for location lookup and uses Yelp API to find restaurants nearby. The app is deployed on GitHub and uses Firebase for database updates',
 		media: 'http://via.placeholder.com/600x400',
 		github: 'https://github.com/thaiscmky/letseat',		
-		livelink: 'https://thaiscmky.github.io/letseat/',
-		technologies: [react, node, node],
+		applink: 'https://thaiscmky.github.io/letseat/',
+		technologies: [node, bootstrap, knockout, jquery, firebase, heroku],
 	},
 ]
 
