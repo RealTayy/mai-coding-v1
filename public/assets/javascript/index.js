cd25c4f4c55be0c2c2ffb99f26c4f906f9b401d2
The Mai <Mai.The.09@gmail.com>
2018-06-29 04:28:16 -0500

    add scrolling animation

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,3 +1,19 @@
 $(() => {
-	console.log('test');
+	console.log('Page JS loaded');
+	$('.hero-island-button > button').on('click', () => {
+		scrollTo('.intro');
+	});
+
+	$('.projects-subheader-text > a').on('click', () => {
+		scrollTo('.contact', true);
+	});
+
+
+	const scrollTo = (selector, offset) => {
+		let newPos = (offset) ? $(selector).offset().top - ($(window).height() / 2.6) : $(selector).offset().top;
+		// console.log(speed);
+		$('html, body').animate({
+			scrollTop: newPos
+		}, 1000, 'easeOutCubic');
+	}
 });
