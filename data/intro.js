7c6d001d0fec95574e3d612f77af7aedbffd2c1e
The Mai <mai.the.09@gmail.com>
2018-07-10 20:33:21 -0500

    do intro stuff

--- data/intro.js
+++ data/intro.js
@@ -1,6 +1,6 @@
 const intro = {
 	header: 'Who am I',
-	text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit ,Lorem ipsum dolor sit amet, consectetur adipiscing elit ,Lorem ipsum dolor sit amet, consectetur adipiscing elit ,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+	text: 'Nice to meet you. My name is The Mai and I\'m a developer living in Houston. I\'m very passionate about doing the things I love. Luckily for me one of those things just so happens to be Web Development. I love making stuff that feels great to use. Want me to make your stuff? Let\'s grab some coffee.'
 };
 
 module.exports = intro;
