1ffbea2a4bd9aa7a0dc2b5913f33a5a17a32e6b8
The Mai <mai.the.09@gmail.com>
2018-07-10 21:05:22 -0500

    finish text details

--- data/intro.js
+++ data/intro.js
@@ -1,6 +1,6 @@
 const intro = {
-	header: 'Who am I',
-	text: 'Nice to meet you. My name is The Mai and I\'m a developer living in Houston. I\'m very passionate about doing the things I love. Luckily for me one of those things just so happens to be Web Development. I love making stuff that feels great to use. Want me to make your stuff? Let\'s grab some coffee.'
+	header: 'Hey there. Let\'s talk.',
+	text: 'Nice to meet you. My name is The Mai and I\'m a developer living in Houston. I\'m very passionate about doing the things I love. Luckily for me one of those things just so happens to be Web Development. I love working with talented people and making stuff that feels great to use. Want me to make your stuff? Let\'s talk over a cup of coffee.'
 };
 
 module.exports = intro;
