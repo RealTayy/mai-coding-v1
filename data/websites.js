dc5ad4ae4f8586b682c67e06f5f9da22495d0f36
The Mai <Mai.The.09@gmail.com>
2018-12-04 15:17:35 -0600

    add animationzone content

--- data/websites.js
+++ data/websites.js
@@ -4,6 +4,12 @@ const websites = [
 		bgimg: './assets/images/websites/phonow.png',
 		minilogo: './assets/images/websites/phonowmini.png',
 		link: 'https://www.phonowtexas.com'
+	},
+	{
+		caption: 'Animation Zone is a collection of libraries that I made to make my life easier.',
+		bgimg: './assets/images/websites/animationzone.png',
+		minilogo: './assets/images/websites/animationzonemini.png',
+		link: 'https://realtayy.github.io/animation-zone/'
 	}
 ]
 
